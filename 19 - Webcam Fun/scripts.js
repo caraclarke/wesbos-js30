c2435e7bbdbfeb3f289dbe050062fe0467a5cc8c
caraclarke <cara.clarke8@gmail.com>
2017-04-19 09:49:26 -0400

    access user video

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,17 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  // get webcam w/ mediaDevices.getUserMedia
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error('hey', err);
+    })
+}
+
+// getVideo();
