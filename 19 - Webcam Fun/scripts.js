c469a21137c99ad5d5e3548f916261a9fa4edcbd
caraclarke <cara.clarke8@gmail.com>
2017-04-19 09:54:06 -0400

    once video is played, grab event and display video on canvas. cv resizing to fit video

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -12,8 +12,28 @@ function getVideo() {
       video.play();
     })
     .catch(err => {
-      console.error('hey', err);
+      console.error('getVideo err: ', err);
     })
 }
 
-// getVideo();
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // pass DI an img or video and will paint to context
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
